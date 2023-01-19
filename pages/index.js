ab519cc9b8f16dca8384d54a2d45b99a433f152f
Federico Blautzik <blautzik.f@gmail.com>
2023-01-19 08:21:23 -0300

    heropost delete

--- pages/index.js
+++ pages/index.js
@@ -23,16 +23,6 @@ export default function Index({ allPosts: initialAllPosts, preview }) {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
           {morePosts.length > 0 && <MoreStories posts={allPosts} />}
         </Container>
       </Layout>
